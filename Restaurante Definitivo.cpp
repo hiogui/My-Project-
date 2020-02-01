cbe36ea964a8a217c2c5da6da9b8eebfb9c5a763
Jorge Gabriel <jorgegabriel99h@gmail.com>
2020-02-01 13:07:15 -0300

    Cod Alterado

--- Restaurante Definitivo.cpp
+++ Restaurante Definitivo.cpp
@@ -35,7 +35,6 @@ noPtr busca_cliente(int x) {
 }
 void pedido() {
     int x;
-    cout << "____________" << endl;
     cout << "Entre com o codigo do cliente: ";
     cin >> x;
  noPtr cli = busca_cliente(x);
